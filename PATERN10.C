f174014142945e54257bdb97f1a89ae978a26680
shubham gupta <36784415+shubhamorai12345@users.noreply.github.com>
2018-11-01 08:16:42 +0530

    *

---
+++ PATERN10.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,j;
+clrscr();
+printf("Enter the number of row");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+	for(j=0;j<=i;j++)
+	{
+	printf(" *");
+	}
+	printf("\n");
+   }
+getch();
+}
