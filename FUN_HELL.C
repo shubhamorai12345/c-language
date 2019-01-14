e8fb248611cf7318d4bcb09429d55afb70800244
shubham gupta <36784415+shubhamorai12345@users.noreply.github.com>
2019-01-14 10:43:11 +0530

    this hello program make with function

---
+++ FUN_HELL.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void myfunction();
+void main()
+{
+printf("My name is shubham gupta");
+myfunction();
+getch();
+}
+void myfunction()
+{
+     printf("\n I am from orai");
+
+ }
